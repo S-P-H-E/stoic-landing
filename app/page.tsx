b56443bc4cadb272768e788239112183244ec54f
S-P-H-E <sphe.g.personal@gmail.com>
2023-10-05 01:50:04 +0200

    &quot; fix

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   const reviews = [
     {
       id: 1,
-      text: "If anyone is thinking about upgrading, do it. Only been in there 15 minutes and it's awesome, so many guides and content to encorporate in your videos, great people to network with as well.",
+      text: "If anyone is thinking about upgrading, do it. Only been in there 15 minutes and it&quot;s awesome, so many guides and content to encorporate in your videos, great people to network with as well.",
       image: "https://cdn.discordapp.com/avatars/444898972046458880/0d57c117707379730e2ffff399fed496.webp?size=80",
       author: "Ozzy"
     },
@@ -21,7 +21,7 @@ export default function Home() {
     },
     {
       id: 3,
-      text: "If you are new to video editing and are looking for a place to receive 1-on-1 help and feedback, well, then you've found the right place. The staff members are welcoming and wonderful to chat with. Not only do they provide assistance, but they also offer creative ideas and supervised tutorials to expedite your learning. The coaching calls have proven quite helpful in receiving personalized feedback on each of my videos.",
+      text: "If you are new to video editing and are looking for a place to receive 1-on-1 help and feedback, well, then you&quot;ve found the right place. The staff members are welcoming and wonderful to chat with. Not only do they provide assistance, but they also offer creative ideas and supervised tutorials to expedite your learning. The coaching calls have proven quite helpful in receiving personalized feedback on each of my videos.",
       image: "https://cdn.discordapp.com/avatars/277460064531644416/be8c9f4771f325a4dd9c6025ea6887b7.webp?size=80",
       author: "Knowledge Kombat"
     },
