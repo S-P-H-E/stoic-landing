818bf1d9e4dd6fc928492caeed27090d3cbf7d74
S-P-H-E <sphe.g.personal@gmail.com>
2023-11-12 11:23:41 +0200

    3d stuff

--- components/Benefits/Card.tsx
+++ components/Benefits/Card.tsx
@@ -3,35 +3,36 @@ import { useRef } from "react"
 import Image from "next/image";
 import { motion } from "framer-motion"
 import clsx from 'clsx';
+import Spline from '@splinetool/react-spline';
 
 export default function Card(){
     const copy = [
         {
-            image: "/images/library.jpg",
+            image: <Spline scene="https://prod.spline.design/oqR80zM7-PZjKO9n/scene.splinecode"/>,
             icon: "/icon/edu.png",
             title: "Up-to-date Courses",
             description: "Get the best and up to date courses on social media from people who are active in the space."
         },
         {
-            image: "/images/community.jpg",
+            image: <Spline scene="https://prod.spline.design/hD1qOIBU7NSTHTPr/scene.splinecode" />,
             icon: "/icon/handshake.png",
             title: "The Best Community",
             description: "You'll have access to an amazing supportive community who you can network with and further."
         },
         {
-            image: "/images/counter.jpg",
+            image: <Spline scene="https://prod.spline.design/ONlJE-1szacZYp0R/scene.splinecode" />,
             icon: "/icon/moneybag.png",
             title: "Huge Monetary Opportunities",
             description: "The Social Media space is a billion dollar industry and we'll be showing you how to get paid a fraction of that."
         },
         {
-            image: "/images/robot.jpg",
+            image: <Spline scene="https://prod.spline.design/VZDR2R47yNAlFJd4/scene.splinecode" />,
             icon: "/icon/clock.png",
             title: "24/7 Support",
             description: "You'll have access to Stoic AI that will answer any of your social media questions even when we are offline."
         },
         {
-            image: "/images/laptop.jpg",
+            image: <Spline scene="https://prod.spline.design/Wsz1EEDwBcpFPVgi/scene.splinecode" />,
             icon: "/icon/clock.png",
             title: "Innovative Platfrom",
             description: "We've built a platform like none other on the market it contains our courses, AI, resources and tools built to assist your journey"
@@ -64,7 +65,11 @@ export default function Card(){
                     }}
                     
                     key={index} className={"border shadow-xl text-black p-10 rounded-3xl flex items-center gap-10 w-[80vw] mx-auto"}>
-                    <Image src={info.image} alt='edu' width={300} height={0} className="rounded-xl"/>
+                        
+                        <div className="w-[300px] h-[300px]">
+                            {info.image}
+                        </div>
+                    {/* <Image src={info.image} alt='edu' width={300} height={0} className="rounded-xl"/> */}
                     <div className={"w-full flex flex-col gap-4"}>
                     {/* <Image src={info.image} alt='edu' width={90} height={0} className="bg-[#181818] p-2 rounded-2xl"/> */}
                         <h1 className="text-4xl font-semibold">{info.title}</h1>
--- components/Courses/page.tsx
+++ components/Courses/page.tsx
@@ -1,7 +1,10 @@
 export default function Courses(){
     return(
-        <div className="p-20 flex justify-center">
+        <div className="p-20 flex flex-col gap-5 items-center">
             <h1 className="text-4xl">Here is what you <mark className="bg-transparent text-white font-semibold">learn</mark></h1>
+            <div className="border border-[#181818] p-10 w-full rounded-3xl">
+                <h1>YAY</h1>
+            </div>
         </div>
     )
 }
