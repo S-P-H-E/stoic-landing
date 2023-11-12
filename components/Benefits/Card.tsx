afb77e9f89a1cf4a2b112a1b97d5493260cbe0b1
S-P-H-E <sphe.g.personal@gmail.com>
2023-11-12 18:19:20 +0200

    icons update

--- components/Benefits/Card.tsx
+++ components/Benefits/Card.tsx
@@ -8,19 +8,19 @@ import Spline from '@splinetool/react-spline';
 export default function Card(){
     const copy = [
         {
-            image: <Spline scene="https://prod.spline.design/oqR80zM7-PZjKO9n/scene.splinecode"/>,
+            image: <Image src='/images/book.png' alt='book' width={300} height={0}/>,
             icon: "/icon/edu.png",
             title: "Up-to-date Courses",
             description: "Get the best and up to date courses on social media from people who are active in the space."
         },
         {
-            image: <Spline scene="https://prod.spline.design/hD1qOIBU7NSTHTPr/scene.splinecode" />,
+            image: <Image src='/images/hand.png' alt='hand' width={300} height={0}/>,
             icon: "/icon/handshake.png",
             title: "The Best Community",
             description: "You'll have access to an amazing supportive community who you can network with and further."
         },
         {
-            image: <Spline scene="https://prod.spline.design/ONlJE-1szacZYp0R/scene.splinecode" />,
+            image: <Image src='/images/money.png' alt='money' width={300} height={0}/>,
             icon: "/icon/moneybag.png",
             title: "Huge Monetary Opportunities",
             description: "The Social Media space is a billion dollar industry and we'll be showing you how to get paid a fraction of that."
@@ -32,7 +32,7 @@ export default function Card(){
             description: "You'll have access to Stoic AI that will answer any of your social media questions even when we are offline."
         },
         {
-            image: <Spline scene="https://prod.spline.design/Wsz1EEDwBcpFPVgi/scene.splinecode" />,
+            image: <Image src='/images/laptop.png' alt='laptop' width={200} height={0}/>,
             icon: "/icon/clock.png",
             title: "Innovative Platfrom",
             description: "We've built a platform like none other on the market it contains our courses, AI, resources and tools built to assist your journey"
@@ -66,9 +66,9 @@ export default function Card(){
                     
                     key={index} className={"border shadow-xl text-black p-10 rounded-3xl flex items-center gap-10 w-[80vw] mx-auto"}>
                         
-                        <div className="w-[300px] h-[300px]">
-                            {info.image}
-                        </div>
+                    <div className="w-[300px] h-[300px] flex items-center justify-center">
+                        {info.image}
+                    </div>
                     {/* <Image src={info.image} alt='edu' width={300} height={0} className="rounded-xl"/> */}
                     <div className={"w-full flex flex-col gap-4"}>
                     {/* <Image src={info.image} alt='edu' width={90} height={0} className="bg-[#181818] p-2 rounded-2xl"/> */}
