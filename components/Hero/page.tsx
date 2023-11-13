00443e8845a41bdceb5bf6fa8d3aecca198d6a58
S-P-H-E <sphe.g.personal@gmail.com>
2023-11-13 11:52:01 +0200

    hero

--- components/Hero/page.tsx
+++ components/Hero/page.tsx
@@ -27,12 +27,12 @@ export default function Hero(){
     return(
         <>
             <div className="h-full w-full absolute gradient-01 -z-10 bottom-[500px]"/>
-            <div className="w-[1500px] mx-auto text-center min-h-[100dvh] flex flex-col justify-center gap-2 m-20">
-                <motion.div style={{y: position, scale: scale, opacity: opacity}} > 
+            <div className="w-[1500px] mx-auto text-center flex flex-col items-center justify-center gap-2 m-20">
+                <motion.div style={{y: position, scale: scale, opacity: opacity}} className="flex flex-col justify-center items-center h-[100dvh]"> 
                     {/* <motion.div style={{ scaleX: scrollYProgress }} className="h-[10px] bg-red-500"/> */}
 
-                    <h1 className="text-4xl md:text-[3vw] p-4 mt-[20vh] uppercase"><mark className="bg-transparent text-white font-semibold">Conquer</mark> The Social Media Algorithm</h1>
-                    <p className="text-sm px-10 md:text-[1.5vw] md:w-[800px] text-center mx-auto">Learn how to get views, get paid and stop wasting time</p>
+                    <h1 className="text-4xl md:text-[3vw] mt-[10vh] p-4 uppercase"><mark className="bg-transparent text-white font-semibold">Conquer</mark> The Social Media Algorithm</h1>
+                    <p className="text-sm px-10 py-2 md:text-[1.4vw] text-center mx-auto">Learn how to get views, get paid and stop wasting time</p>
                     
                     <motion.div className="w-[50vw] -z-0 m-5 aspect-video mx-auto">
                         <div style={{visibility: visibility}} className="absolute w-[50vw] aspect-video bg-black/60 flex items-center justify-center cursor-pointer rounded-3xl" onClick={handlePlay}>
