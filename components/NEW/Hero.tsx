560b3ac9a897ca858bd7675544f8d0df7f9e1f7b
DarKS4ys <melihyardim1057@gmail.com>
2023-12-23 14:43:51 +0300

    fade video

--- components/NEW/Hero.tsx
+++ components/NEW/Hero.tsx
@@ -2,6 +2,7 @@
 
 import Image from 'next/image';
 import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 
 type Visibility = 'visible' | 'hidden';
 
@@ -34,20 +35,28 @@ export default function Hero() {
         <div className="bg-gradient-to-t from-[#0140fd] to-transparent to-95% absolute w-full h-full z-10 opacity-20"/>
         <Image src={Landing} className=" w-[90vw] border-[5px] border-b-0 rounded-b-none rounded-xl border-white/10 drop-shadow-md" alt='lanidng' placeholder="blur"/>
     </div> */}
-      <div className="w-[50vw] -z-0 m-5 aspect-video mx-auto">
-        <div
-          style={{ visibility: visibility }}
-          className="absolute w-[50vw] aspect-video bg-black/60 flex items-center justify-center cursor-pointer rounded-3xl"
-          onClick={handlePlay}
+      <div className="w-[50vw] relative max-w-7xl z-20 m-5 aspect-video mx-auto">
+        <motion.div
+          className="w-full h-full"
+          initial={{ opacity: 0, }}
+          animate={{ opacity: 1, }}
+          transition={{ stiffness: 85, type: 'spring' }}
         >
-          <Image src="/images/play.png" alt="play" width={50} height={0} />
-        </div>
-        <iframe
-          className="rounded-3xl shadow-xl  w-full h-full"
-          src={`https://customer-hyo06dqr7c3pgrtr.cloudflarestream.com/a07c199380af93ce7c2744ca470972ad/iframe?${controls}${muted}&preload=true&autoplay=true&loop=true&poster=https%3A%2F%2Fcustomer-hyo06dqr7c3pgrtr.cloudflarestream.com%2Fa07c199380af93ce7c2744ca470972ad%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600`}
-          allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
-          allowFullScreen={true}
-        ></iframe>
+          <div
+            style={{ visibility: visibility }}
+            className="z-20 absolute w-full aspect-video bg-black/60 flex items-center justify-center cursor-pointer rounded-2xl"
+            onClick={handlePlay}
+          >
+            <Image src="/images/play.png" alt="play" width={50} height={0} />
+          </div>
+          <iframe
+            className="rounded-2xl shadow-xl w-full h-full"
+            src={`https://customer-hyo06dqr7c3pgrtr.cloudflarestream.com/a07c199380af93ce7c2744ca470972ad/iframe?${controls}${muted}&preload=true&autoplay=true&loop=true&poster=https%3A%2F%2Fcustomer-hyo06dqr7c3pgrtr.cloudflarestream.com%2Fa07c199380af93ce7c2744ca470972ad%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600`}
+            allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
+            allowFullScreen={true}
+          ></iframe>
+        </motion.div>
+        <div className="absolute top-0 w-[50vw] max-w-7xl rounded-2xl aspect-video mx-auto bg-neutral-800 animate-pulse -z-10" />
       </div>
     </div>
   );
