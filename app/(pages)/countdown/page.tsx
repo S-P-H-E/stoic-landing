7a3d6f5c76dda9c8499ed1a8d6649bdad535ab25
S-P-H-E <sphe.g.personal@gmail.com>
2024-01-30 18:56:52 +0200

    countdown new

--- app/(pages)/countdown/page.tsx
+++ app/(pages)/countdown/page.tsx
@@ -1,5 +1,8 @@
 "use client"
+import clsx from 'clsx';
 import { useState, useEffect } from 'react';
+import { FiArrowUpRight } from 'react-icons/fi';
+import { MdLock } from "react-icons/md";
 
 export default function Countdown() {
     const targetDate = new Date('February 5, 2024'); // Set the year appropriately
@@ -16,32 +19,43 @@ export default function Countdown() {
     return (
         <div className="flex flex-col h-screen justify-center items-center">
             <div className="fixed top-0 left-0 w-screen h-screen overflow-hidden z-[-1]">
-    <video 
+    {/* <video 
         src="10.mp4" 
         autoPlay 
         loop 
         muted 
         className="absolute min-w-full min-h-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover grayscale blur-sm opacity-50"
-    ></video>
+    ></video> */}
+    <img src='pattern.png' className='absolute min-w-full min-h-full blur-sm' />
 </div>
 
-            <h1 className='text-3xl font-semibold pb-5'>February 5, 2024</h1>
-            <div className='flex bg-[--text] p-4 rounded-2xl gap-4'>
-                <div className='flex flex-col items-center bg-[--bg] p-4 rounded-md w-[100px]'>
-                    <p className='text-5xl font-semibold'>{timeLeft.days}</p>
+            {/* <h1 className='text-3xl font-semibold pb-5'>February 5, 2024</h1> */}
+            <div className='flex bg-black/50 backdrop-blur-sm p-6 rounded-2xl gap-4 border border-[#141414]'>
+                <div className='flex flex-col items-center p-6 rounded-md w-[100px]'>
+                    <p className='text-7xl font-semibold'>{timeLeft.days}</p>
                     Days    
                 </div> 
-                <div className='flex flex-col items-center bg-[--bg] p-4 rounded-md w-[100px]'>
-                    <p className='text-5xl font-semibold'>{timeLeft.hours} </p>Hours
+                <div className='flex flex-col items-center p-6 rounded-md w-[100px]'>
+                    <p className='text-7xl font-semibold'>{timeLeft.hours} </p>Hours
                 </div> 
-                <div className='flex flex-col items-center bg-[--bg] p-4 rounded-md w-[100px]'>
-                    <p className='text-5xl font-semibold'>{timeLeft.minutes}</p> Minutes
+                <div className='flex flex-col items-center p-6 rounded-md w-[100px]'>
+                    <p className='text-7xl font-semibold'>{timeLeft.minutes}</p> Minutes
                 </div> 
-                <div className='flex flex-col items-center bg-[--bg] p-4 rounded-md w-[100px]'>
-                    <p className='text-5xl font-semibold'>{timeLeft.seconds}</p> Seconds
+                <div className='flex flex-col items-center p-6 rounded-md w-[100px]'>
+                    <p className='text-7xl font-semibold'>{timeLeft.seconds}</p> Seconds
                 </div>
             </div>
-            <button className='bg-[--text] m-5 px-4 py-2 rounded-full text-[--bg] font-semibold'>RESERVE YOUR SPOT</button>
+            <button
+                className={clsx('bg-[#181818] transition-all border-b px-7 py-4 my-5 text-white text-xl flex items-center gap-2 w-fit duration-300 work-sans')}
+                
+                >
+                RESERVE YOUR SPOT
+                <FiArrowUpRight className="rotate-45" />
+            </button>
+            <div className='text-[#ffffff] flex items-center gap-2'>
+                <MdLock />
+                <p className='text-[14px]'>INCLUDES ACCESS TO ALL COURSES</p>
+            </div>
         </div>
 
     );
