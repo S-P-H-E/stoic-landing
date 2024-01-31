cab6ef07b65371eca9baefa2ba9b8f8ec813445b
DarKS4ys <melihyardim1057@gmail.com>
2024-01-31 15:51:00 +0300

    New button and 404

--- app/(pages)/countdown/page.tsx
+++ app/(pages)/countdown/page.tsx
@@ -1,19 +1,15 @@
 "use client"
 
 import React, {useState, useEffect, useTransition, useMemo} from 'react';
-import {FiArrowUpRight, FiLoader} from 'react-icons/fi';
 import {MdLock} from "react-icons/md";
 import patternImage from '@/public/pattern.png'
 import Image from "next/image";
-import {handleSubscription} from "@/app/lib/utils";
-import {useRouter} from "next/navigation";
+import CountdownButton from "@/components/CountdownButton";
+
 
 export default function Countdown() {
     const targetDate = useMemo(() => new Date('February 5, 2024'), []); // Memoize the targetDate initialization
     const [timeLeft, setTimeLeft] = useState(calculateTimeLeft(targetDate));
-    const [isPending, startTransition] = useTransition()
-
-    const router = useRouter()
 
     useEffect(() => {
         // Run this only on the client side
@@ -24,19 +20,6 @@ export default function Countdown() {
         // Clean up the interval when the component unmounts
         return () => clearInterval(timer);
     }, [targetDate]);
-
-    const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        startTransition(async () => {
-            try {
-                e.preventDefault();
-
-                const data = await handleSubscription(e);
-                router.push(data);
-            } catch (error: any) {
-                console.error('Error during subscription:', error.message);
-            }
-        });
-    };
 
     return (
         <div className="flex flex-col h-screen justify-center items-center">
@@ -69,16 +52,7 @@ export default function Countdown() {
                 </div>
 
             </div>
-            <button
-                onClick={handleButtonClick}
-                disabled={isPending}
-                className='disabled:opacity-50 bg-[#181818] hover:bg-white hover:text-black active:scale-95 transition-all border-b px-7 py-4 my-5 text-white text-xl flex items-center gap-2 w-fit duration-300 work-sans'
-            >
-                {isPending ?
-                    <div className="flex items-center gap-2"><FiLoader className="animate-spin"/> RESERVING YOUR SPOT
-                    </div> : 'RESERVE YOUR SPOT'}
-                <FiArrowUpRight className="rotate-45"/>
-            </button>
+            <CountdownButton/>
             <div className='text-[#ffffff] flex items-center gap-2'>
                 <MdLock/>
                 <p className='text-[14px]'>INCLUDES ACCESS TO ALL COURSES</p>
---
+++ components/CountdownButton.tsx
@@ -0,0 +1,98 @@
+import React, {useRef, useState, useTransition} from "react";
+import {FiArrowUpRight, FiLoader, FiLock} from "react-icons/fi";
+import {motion} from "framer-motion";
+import {useRouter} from "next/navigation";
+import {handleSubscription} from "@/app/lib/utils";
+
+const TARGET_TEXT = "RESERVE  YOUR SPOT";
+const CYCLES_PER_LETTER = 2;
+const SHUFFLE_TIME = 28;
+
+const CHARS = "\!21@#$%^&*():{};|,.<>2/?";
+
+const CountdownButton = () => {
+    const intervalRef = useRef<ReturnType<typeof setInterval> | null>(null);
+    const [text, setText] = useState(TARGET_TEXT);
+    const [isPending, startTransition] = useTransition()
+
+    const router = useRouter()
+    const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        startTransition(async () => {
+            try {
+                e.preventDefault();
+
+                const data = await handleSubscription(e);
+                router.push(data);
+            } catch (error: any) {
+                console.error('Error during subscription:', error.message);
+            }
+        });
+    };
+
+
+    const scramble = () => {
+        let pos = 0;
+
+        intervalRef.current = setInterval(() => {
+            const scrambled = TARGET_TEXT.split("")
+                .map((char, index) => {
+                    if (pos / CYCLES_PER_LETTER > index) {
+                        return char;
+                    }
+
+                    const randomCharIndex = Math.floor(Math.random() * CHARS.length);
+                    return CHARS[randomCharIndex];
+                })
+                .join("");
+
+            setText(scrambled);
+            pos++;
+
+            if (pos >= TARGET_TEXT.length * CYCLES_PER_LETTER) {
+                stopScramble();
+            }
+        }, SHUFFLE_TIME);
+    };
+
+    const stopScramble = () => {
+        clearInterval(intervalRef.current || undefined);
+
+        setText(TARGET_TEXT);
+    };
+
+    return (
+        <motion.button
+            whileHover={{
+                scale: 1.025,
+            }}
+            whileTap={{
+                scale: 0.975,
+            }}
+            onMouseEnter={scramble}
+            onMouseLeave={stopScramble}
+            className="group relative overflow-hidden disabled:opacity-50 bg-[#181818] duration-200 active:scale-95 hover:bg-white hover:text-black transition-all border-b px-7 py-4 my-5 text-white text-xl flex items-center gap-2 work-sans w-80 justify-center"
+        >
+            <div className="relative z-10 flex items-center gap-2">
+                <span>{isPending ? <FiLoader className="animate-spin" /> : text}</span>
+                <FiArrowUpRight className="rotate-45"/>
+            </div>
+            <motion.span
+                initial={{
+                    y: "100%",
+                }}
+                animate={{
+                    y: "-100%",
+                }}
+                transition={{
+                    repeat: Infinity,
+                    repeatType: "mirror",
+                    duration: 1,
+                    ease: "linear",
+                }}
+                className="duration-300 absolute inset-0 z-0 scale-125 bg-gradient-to-t from-transparent from-50% via-white group-hover:via-black/50 to-transparent to-60% opacity-0 transition-opacity group-hover:opacity-100"
+            />
+        </motion.button>
+    );
+};
+
+export default CountdownButton;
