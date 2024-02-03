d7da255fdb8b360d11874b2c2914683a604f4c6a
DarKS4ys <melihyardim1057@gmail.com>
2024-02-03 17:39:24 +0300

    fix type and animation

--- components/Courses.tsx
+++ components/Courses.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Playfair} from "next/font/google";
 import clsx from "clsx";
+import {motion} from 'framer-motion'
 
 const font = Playfair({
     subsets: ['latin'],
@@ -8,6 +9,20 @@ const font = Playfair({
 })
 
 const MyComponent = () => {
+
+    const fadeInAnimationVariants = {
+        initial: {
+            opacity: 0,
+            y: 100,
+        },
+        animate: (index: number) => ({
+            opacity: 1,
+            y: 0,
+            transition: {
+                delay: 0.08 * index,
+            },
+        }),
+    };
 
     const courses = [
         {
@@ -23,14 +38,22 @@ const MyComponent = () => {
     ]
 
     return (
-        <div id={'courses'} className="scroll-my-20 h-full flex flex-col items-center justify-center relative max-w-8xl gap-8 mx-auto">
+        <motion.div id={'courses'} initial={{opacity: 0}} whileInView={{opacity: 1}} viewport={{once:true}} className="scroll-my-20 h-full flex flex-col items-center justify-center relative max-w-8xl gap-8 mx-auto">
             <p className="text-[--description] tracking-widest">THE BEST INFORMATION</p>
             <h1 className={clsx("text-8xl font-medium", font.className)}>
                 <mark className="bg-transparent text-white">What you will learn</mark>
             </h1>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-            {courses.map((course, index) => (
-                    <div className="rounded-lg overflow-hidden border border-[--border] flex flex-col">
+                {courses.map((course, index) => (
+                    <motion.div key={index}
+                                custom={index}
+                                variants={fadeInAnimationVariants}
+                                initial="initial"
+                                whileInView="animate"
+                                viewport={{
+                                    once: true,
+                                }}
+                                className="rounded-lg overflow-hidden border border-[--border] flex flex-col">
                         <div className="bg-[#161616] justify-center text-center items-center p-8 flex flex-col gap-2">
                             <h1 className="text-2xl">{course.title}</h1>
                             <h2 className="text-[--gray] font-light">{course.description}</h2>
@@ -43,10 +66,10 @@ const MyComponent = () => {
                                 className="w-full"
                             />
                         </div>
-                    </div>
+                    </motion.div>
                 ))}
             </div>
-        </div>
+        </motion.div>
     );
 };
 
