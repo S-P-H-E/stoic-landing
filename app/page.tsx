c904e868b77b4573091cb053a59f3c61222e848d
DarKS4ys <melihyardim1057@gmail.com>
2024-02-03 17:36:20 +0300

    test courses

--- app/page.tsx
+++ app/page.tsx
@@ -12,29 +12,9 @@ import About from "@/components/About";
 import Features2 from "@/components/Features2";
 import BannerMessage from "@/components/BannerMessage";
 import Slideshow from "@/components/Slideshow";
+import Courses from "@/components/Courses";
 
 export default function Home() {
-
-    const reviews = [
-        {
-            id: 1,
-            text: "If anyone is thinking about upgrading, do it. Only been in there 15 minutes and it's awesome, so many guides and content to incorporate in your videos, great people to network with as well.",
-            image: "https://cdn.discordapp.com/avatars/444898972046458880/0d57c117707379730e2ffff399fed496.webp?size=80",
-            author: "Ozzy"
-        },
-        {
-            id: 2,
-            text: "Excellent! Taking the guide with you guys was the best idea and investment I made, really, really, really thanks so much",
-            image: "https://cdn.discordapp.com/avatars/1121888745092812871/d36a6e750875da0ecd8799282fb9dcd9.webp?size=80",
-            author: "The Last Rebel"
-        },
-        {
-            id: 3,
-            text: "If you are new to video editing and are looking for a place to receive 1-on-1 help and feedback, well, then you've found the right place. The staff members are welcoming and wonderful to chat with. Not only do they provide assistance, but they also offer creative ideas and supervised tutorials to expedite your learning. The coaching calls have proven quite helpful in receiving personalized feedback on each of my videos.",
-            image: "https://cdn.discordapp.com/avatars/277460064531644416/be8c9f4771f325a4dd9c6025ea6887b7.webp?size=80",
-            author: "Knowledge Kombat"
-        },
-    ];
 
     return (
         <>
@@ -42,6 +22,7 @@ export default function Home() {
             <Hero/>
             <About />
             <Features2 />
+            <Courses/>
             <BannerMessage />
             <Slideshow />
             <Reviews/>
---
+++ components/Courses.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {Playfair} from "next/font/google";
+import clsx from "clsx";
+
+const font = Playfair({
+    subsets: ['latin'],
+    weight: ['500'],
+})
+
+const MyComponent = () => {
+
+    const courses = [
+        {
+            title: 'After Effects Crash Course',
+            description: 'Learn everything you need to know about After Effects.',
+            link: 'https://player.vimeo.com/video/901189185?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479'
+        },
+        {
+            title: 'Short Form Masterclass',
+            description: 'Get the best lessons on Short Form Content',
+            link: 'https://player.vimeo.com/video/866449243?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479'
+        }
+    ]
+
+    return (
+        <div id={'courses'} className="scroll-my-20 h-full flex flex-col items-center justify-center relative max-w-8xl gap-8 mx-auto">
+            <p className="text-[--description] tracking-widest">THE BEST INFORMATION</p>
+            <h1 className={clsx("text-8xl font-medium", font.className)}>
+                <mark className="bg-transparent text-white">What you will learn</mark>
+            </h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            {courses.map((course, index) => (
+                    <div className="rounded-lg overflow-hidden border border-[--border] flex flex-col">
+                        <div className="bg-[#161616] justify-center text-center items-center p-8 flex flex-col gap-2">
+                            <h1 className="text-2xl">{course.title}</h1>
+                            <h2 className="text-[--gray] font-light">{course.description}</h2>
+                        </div>
+                        <div className="relative h-[110%] aspect-video w-full">
+                            <iframe
+                                src={course.link}
+                                allow="autoplay; fullscreen; picture-in-picture;"
+                                style={{width: '100%', height: '100%'}}
+                                className="w-full"
+                            />
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default MyComponent;
