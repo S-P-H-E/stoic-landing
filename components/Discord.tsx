7dd9dba8424fde0a7eb44f6c0ec213b3915daaf8
DarKS4ys <melihyardim1057@gmail.com>
2024-03-11 00:14:44 +0300

    small update animation

--- components/Discord.tsx
+++ components/Discord.tsx
@@ -1,18 +1,24 @@
+"use client"
 import Link from 'next/link';
 import React from 'react';
 import { SiDiscord } from 'react-icons/si';
+import {motion, spring} from 'framer-motion'
 
 export default function Discord() {
   const inviteLink = 'https://discord.gg/stoiccord';
   return (
-    <Link
+<motion.div className="z-[999] active:scale-90 fixed bottom-5 right-5" viewport={{once:true}} transition={{duration: .35, type: 'spring', damping: 15, stiffness: 200}}  initial={{y: 30, opacity: 0}} whileInView={{y: 0, opacity: 1}}>
+<Link
+className="flex gap-2 items-center bg-light-gray hover:bg-border transition md:px-4 p-2 rounded-lg border border-border"
       href={inviteLink}
-      target='_blank'
-      className="z-[999] flex gap-2 items-center bg-light-gray hover:bg-border transition active:scale-90 fixed bottom-5 right-5 px-4 py-2 rounded-lg border border-border"
+      target="_blank"
+      
     >
-      <SiDiscord />
-      Join the community!
+      <SiDiscord size={22}/>
+      <p className="font-medium md:flex hidden">Join the community!</p>
       <div className="absolute -top-1 -right-1 w-4 h-4 bg-emerald-500 animate-ping rounded-full " />
-    </Link>
+    </Link>  
+</motion.div>
+
   );
 }
